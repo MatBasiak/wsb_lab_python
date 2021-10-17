0483a3b549b09d1f63f375a69115d57f6077023a
Mateusz Basiak <mateusz.basiak@gmail.com>
2021-10-17 14:51:44 +0200

    zad 4 refactor, ' added to string value

--- lista 1 zad 3.py
+++ lista 1 zad 3.py
@@ -1,10 +1,22 @@
 def accumulate(objects):
-    accumulator = objects[0]
-    for idx, obj in enumerate(objects):
-        yield accumulator
-        if idx < len(objects) - 1:
-            accumulator += objects[idx + 1]
+    if type(objects[0]) == str:
+        accumulator = objects[0]
+        for idx, obj in enumerate(objects):
+            yield "'" + accumulator + "'"
+            if idx < len(objects) - 1:
+                accumulator += objects[idx + 1]
+
+    else:
+        accumulator = objects[0]
+        for idx, obj in enumerate(objects):
+            yield accumulator
+            if idx < len(objects) - 1:
+                accumulator += objects[idx + 1]
 
 
-for acc in accumulate(["ala ", "ma ", "kota "]):
+for acc in accumulate(["ala ", "ma", " kota"]):
+        print(acc, end=", ")
+
+print("\n")
+for acc in accumulate([1, 2, 3, 4, 5]):
     print(acc, end=" ")
