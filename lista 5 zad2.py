2cf07fcd0c541cc37c891610bc84e220ebabd53b
Mateusz Basiak <mateusz.basiak@gmail.com>
2021-11-15 20:46:17 +0100

    lista 5 zad 1 i 2

---
+++ lista 5 zad2.py
@@ -0,0 +1,15 @@
+def tetranacci(n):
+    tetra = [0, 0, 0, 1, ]
+
+    if n in (1, 2, 3, 4):
+        return tetra[n - 1]
+    else:
+        return tetranacci(n - 1) + tetranacci(n - 2) + tetranacci(n - 3) + tetranacci(n - 4)
+
+
+def main():
+    x = tetranacci(12)
+    print(x)
+
+if __name__ == '__main__':
+    main()
